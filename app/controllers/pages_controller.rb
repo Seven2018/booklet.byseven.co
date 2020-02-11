4c6ba50e4be37820dd56f86edc3289abec8393eb
Brice Chapuis <brice.chapuis@gmail.com>
2020-02-11 14:22:44 +0100

    Catalogue filter done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,19 +24,19 @@ class PagesController < ApplicationController
   def catalogue
     # Index with 'search' option and global visibility for SEVEN Users
     if current_user.access_level == 'Super Admin'
-      @training_programs = TrainingProgram.all
-      @workshops = Workshop.all
+      @training_programs = TrainingProgram.all.order(title: :asc)
+      @workshops = Workshop.all.order(title: :asc)
       if params[:search]
         @training_programs = ((TrainingProgram.where("lower(title) LIKE ?", "%#{params[:search][:title].downcase}%").order(title: :asc)) + (TrainingProgram.joins(program_workshops: :workshop).where("lower(workshops.title) LIKE ?", "%#{params[:search][:title].downcase}%"))).flatten(1).uniq
         @workshops = Workshop.where("lower(title) LIKE ?", "%#{params[:search][:title].downcase}%").order(title: :asc)
       elsif params[:filter]
-        @training_programs = TrainingProgram.joins(workshops: :workshop_categories).where(workshop_categories: {category_id: params[:filter].map(&:to_i)})
-        @workshops = Workshop.joins(:workshop_categories).where(workshop_categories: {category_id: params[:filter].map(&:to_i)})
+        @training_programs = TrainingProgram.joins(workshops: :workshop_categories).where(workshop_categories: {category_id: params[:filter].map(&:to_i)}).order(title: :asc)
+        @workshops = Workshop.joins(:workshop_categories).where(workshop_categories: {category_id: params[:filter].map(&:to_i)}).order(title: :asc)
       end
     # Index for other Users, with visibility limited to programs proposed by their company only
     else
-      @training_programs = TrainingProgram.joins(:company).where(companies: { name: current_user.company.name })
-      @workshops = Workshop.where(company_id: current_user.company.id)
+      @training_programs = TrainingProgram.joins(:company).where(companies: { name: current_user.company.name }).order(title: :asc)
+      @workshops = Workshop.where(company_id: current_user.company.id).order(title: :asc)
       if params[:search]
         @training_programs = @training_programs.where("lower(title) LIKE ?", "%#{params[:search][:title].downcase}%").order(title: :asc)
       end
@@ -60,9 +60,22 @@ class PagesController < ApplicationController
     end
   end
 
-  def filter_catalogue
-    category_ids = params[:workshop][:category_ids].drop(1).map(&:to_i)
-    redirect_to catalogue_path(filter: category_ids)
+  def catalogue_filter_workshop
+    @workshop_categories = params[:workshop][:category_ids].drop(1).map(&:to_i)
+    @workshop_categories = Category.where(company_id: current_user.company_id).map(&:id) if params[:filter][:all] == '1'
+    respond_to do |format|
+      format.html {redirect_to catalogue_path}
+      format.js
+    end
+  end
+
+  def catalogue_filter_program
+    @program_categories = params[:training_program][:category_ids].drop(1).map(&:to_i)
+    @program_categories = Category.where(company_id: current_user.company_id).map(&:id) if params[:filter][:all] == '1'
+    respond_to do |format|
+      format.html {redirect_to catalogue_path}
+      format.js
+    end
   end
 
   %w(catalogue_workshops_title_order_asc catalogue_workshops_title_order_desc catalogue_workshops_type_order_asc catalogue_workshops_type_order_desc catalogue_workshops_duration_order_asc catalogue_workshops_duration_order_desc ).each do |name|
