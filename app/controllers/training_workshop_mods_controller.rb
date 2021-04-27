dfb6c518c72d1196a61a60bbfa670d255cd2541d
Brice Chapuis <bricechapuis>
2021-04-27 11:52:39 +0200

    bugs fixes

--- app/controllers/training_workshop_mods_controller.rb
+++ app/controllers/training_workshop_mods_controller.rb
@@ -5,11 +5,13 @@ class TrainingWorkshopModsController < ApplicationController
     authorize @training_workshop_mod
     # Links TrainingWorkshop to Mods through joined table
     array = params[:training_workshop][:mod_ids].drop(1).map(&:to_i)
+    last_position = TrainingWorkshopMod.where(training_workshop_id: @training_workshop.id)&.order(position: :asc)&.last&.position
+    last_position = 1 unless last_position.present?
     array.each do |ind|
       if TrainingWorkshopMod.where(training_workshop_id: @training_workshop.id, mod_id: ind).empty?
-    raise
-        TrainingWorkshopMod.create(training_workshop_id: @training_workshop.id, mod_id: ind)
+        TrainingWorkshopMod.create(training_workshop_id: @training_workshop.id, mod_id: ind, position: last_position)
       end
+      last_position += 1
     end
     # Select all Categories whose checkbox is unchecked and destroy their TrainingWorkshopMod, if existing
     (Mod.ids - array).each do |ind|
@@ -17,6 +19,8 @@ class TrainingWorkshopModsController < ApplicationController
         TrainingWorkshopMod.where(training_workshop_id: @training_workshop.id, mod_id: ind).first.destroy
       end
     end
+    i = 1
+    TrainingWorkshop.where(training_workshop_id: @training_workshop.id).order(position: :asc).each{|c| c.update(position: i); i += 1;}
     redirect_to training_workshop_path(@training_workshop)
   end
 end
