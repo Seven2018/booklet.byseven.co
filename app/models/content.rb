2724c4bb9a2baab9edbcef41524be0241d5ccfad
Brice Chapuis <bricechapuis>
2021-06-04 16:52:12 +0200

    ...

--- app/models/content.rb
+++ app/models/content.rb
@@ -7,6 +7,7 @@ class Content < ApplicationRecord
   has_many :content_skills, dependent: :destroy
   has_many :skills, through: :content_skills
   has_many :mods, dependent: :destroy
+  has_many :sessions, dependent: :destroy
   has_many :assessments, dependent: :destroy
   has_many :user_interests, dependent: :destroy
   has_many :interested, through: :user_interests, source: :user
