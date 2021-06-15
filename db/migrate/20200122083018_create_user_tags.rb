950d87f2edc47113caeec6dd9bc6e7f2099af222
Brice Chapuis <bricechapuis>
2021-06-15 15:20:35 +0200

    Misc bug fixes

--- db/migrate/20200122083018_create_user_tags.rb
+++ db/migrate/20200122083018_create_user_tags.rb
@@ -3,6 +3,7 @@ class CreateUserTags < ActiveRecord::Migration[6.0]
     create_table :user_tags do |t|
       t.references :user, foreign_key: true
       t.references :tag, foreign_key: true
+      t.references :tag_category, foreign_key: true
       t.timestamps
     end
   end
