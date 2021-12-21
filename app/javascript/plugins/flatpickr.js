22cdcc83e2038ae2227af81dbcaee605bfaa0594
Hadrien Matringe <hadrien.matringe@gmail.com>
2021-12-21 10:58:23 +0100

    Create .datepicker-defaulting-to-end-of-month

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,9 +2,20 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
+const lastDayOfMonth = () => {
+  const today = new Date
+  return new Date(today.getFullYear(), today.getMonth() + 1, 0)
+}
+
 flatpickr(".datepicker", {
   disableMobile: true,
   dateFormat: "d/m/Y",
+})
+
+flatpickr(".datepicker-defaulting-to-end-of-month", {
+  disableMobile: true,
+  dateFormat: "d/m/Y",
+  defaultDate: lastDayOfMonth()
 })
 
 flatpickr(".datepicker-range", {
