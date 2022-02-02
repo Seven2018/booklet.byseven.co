74150598757d7947945b6a3db9712d48e2d3b396
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-02-02 22:25:52 +0100

    Require bundler in application.rb as per create_api_gem doc

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+require 'bundler'
 Bundler.require(*Rails.groups)
 
 module BookletBysevenCo
