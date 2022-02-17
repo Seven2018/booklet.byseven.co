801d25dbccb64ce6d9a1ca216231a29aadc837eb
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-02-17 18:11:35 +0100

    Create Poro::Campaign

---
+++ app/models/poro/campaign.rb
@@ -0,0 +1,24 @@
+module Poro
+  class Campaign
+    def initialize(campaign:, employee_id:)
+      @campaign = campaign
+      @employee_id = employee_id
+    end
+
+    def employee_interview
+      @campaign.interviews.where(employee_id: @employee_id).find(&:employee?)
+    end
+
+    def manager_interview
+      if @campaign.simple?
+        return @campaign.interviews.where(employee_id: @employee_id).find(&:simple?)
+      end
+
+      @campaign.interviews.where(employee_id: @employee_id).find(&:manager?)
+    end
+
+    def crossed_interview
+      @campaign.interviews.where(employee_id: @employee_id).find(&:crossed?)
+    end
+  end
+end
