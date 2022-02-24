945a1414d65b71143e9a3e2110869b641a5a79e5
Brice Chapuis <bricechapuis>
2022-02-24 09:33:11 +0100

    Interview Form edit left and center parts done

---
+++ db/migrate/20220223175148_add_required_for_to_interview_question.rb
@@ -0,0 +1,5 @@
+class AddRequiredForToInterviewQuestion < ActiveRecord::Migration[6.0]
+  def change
+    add_column :interview_questions, :required_for, :integer, default: 0, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_194007) do
+ActiveRecord::Schema.define(version: 2022_02_23_175148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -235,6 +235,7 @@ ActiveRecord::Schema.define(version: 2022_02_15_194007) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "visible_for", default: 0, null: false
+    t.integer "required_for", default: 0, null: false
     t.index ["interview_form_id"], name: "index_interview_questions_on_interview_form_id"
   end
 
