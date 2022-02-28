dea758b86f45ecec359c61b9b2d209173dfeb951
damien jouaneau <damienjouaneau@gmail.com>
2022-02-28 14:36:35 +0100

    reste un fix concernant le questionnaire pour que la question sélectionnée sur le menu gauche s'affiche au milieu sur le menu de droite

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -231,6 +231,10 @@ class CampaignsController < ApplicationController
 
   def edit
     authorize @campaign
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
