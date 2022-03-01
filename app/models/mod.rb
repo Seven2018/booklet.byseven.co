ba9a8bdbc19e075d4a30732bcb9d035c08097fee
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-03-01 14:28:37 +0100

    Create Mod :valid_video and spec

--- app/models/mod.rb
+++ app/models/mod.rb
@@ -5,4 +5,15 @@ class Mod < ApplicationRecord
   has_many :user_forms, dependent: :destroy
   validates :title, presence: true
   has_rich_text :text
+  validate :valid_video
+
+  private
+
+  def valid_video
+    return true if video.blank?
+
+    return true if video.include? 'www.youtube.com/watch?v='
+
+    errors.add(:video, 'invalid video link')
+  end
 end
---
+++ spec/factories/mod.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :mod do
+    title { 'rand title' }
+  end
+end
