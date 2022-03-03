39a663981d16735582c39b2e13b220817785e759
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-03-03 14:39:28 +0100

    Guard 20220223175148 migration from duplicate on outdated_master_production branch

--- db/migrate/20220223175148_add_required_for_to_interview_question_and_answerable_by_to_interview_form.rb
+++ db/migrate/20220223175148_add_required_for_to_interview_question_and_answerable_by_to_interview_form.rb
@@ -1,7 +1,13 @@
 class AddRequiredForToInterviewQuestionAndAnswerableByToInterviewForm < ActiveRecord::Migration[6.0]
   def change
-    add_column :interview_questions, :required_for, :integer, default: 0, null: false
-    add_column :interview_forms, :answerable_by, :integer, default: 0, null: false
-    add_column :interview_forms, :cross, :boolean, default: false
+    unless column_exists?(:interview_questions, :required_for)
+      add_column :interview_questions, :required_for, :integer, default: 0, null: false
+    end
+    unless column_exists?(:interview_forms, :answerable_by)
+      add_column :interview_forms, :answerable_by, :integer, default: 0, null: false
+    end
+    unless column_exists?(:interview_forms, :cross)
+      add_column :interview_forms, :cross, :boolean, default: false
+    end
   end
 end
