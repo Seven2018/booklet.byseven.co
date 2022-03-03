55f26798e750ca7dfa2f6aa65d01e1c00f1946cf
Brice Chapuis <bricechapuis>
2022-03-03 14:10:44 +0100

    Catalogue done (contents only)

--- app/models/time_conversion.rb
+++ app/models/time_conversion.rb
@@ -2,6 +2,6 @@ module TimeConversion
   def self.convert(minutes)
     hours = minutes / 60
     rest = minutes % 60
-    return "#{hours}h#{rest unless rest == 0}"
+    return "#{hours}h#{format('%02d', rest) unless rest == 0}"
   end
 end
