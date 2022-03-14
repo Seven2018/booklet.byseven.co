abbd800695e7ead6a69d8c8a084e1ce126f98134
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-03-14 17:01:15 +0100

    Fix trix contents edit

--- app/components/trix_input_component.rb
+++ app/components/trix_input_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class TrixInputComponent < ViewComponent::Base
-  def initialize(name:, value:, placeholder:)
+  def initialize(name:, value:, placeholder:, klasses:)
     @name = name
     @value = value
     @placeholder = placeholder
+    @klasses = klasses
     @rand_uuid = "#{('a'..'z').to_a.sample}#{rand(10_000)}"
   end
 end
