083efa77260eaa50ad4d988568141ae54a5877ba
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-03-15 08:36:18 +0100

    Update RadioButtonTileComponent and icons

--- app/components/radio_button_tile_component.rb
+++ app/components/radio_button_tile_component.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class RadioButtonTileComponent < ViewComponent::Base
-  def initialize(model:, name:, checked:, icon_klasses:, title:, subtitle:, disabled: false, compact: false)
+  def initialize(model:, name:, checked:, title:, subtitle:, disabled: false, compact: false)
     @model = model
     @name = name
     @checked = checked
-    @icon_klasses = icon_klasses
     @title = title
     @subtitle = subtitle
     @disabled = disabled
