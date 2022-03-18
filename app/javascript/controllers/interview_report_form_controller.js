645781948474857c017679e94e7559c7a51cc54c
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-03-18 18:47:18 +0100

    test html stimulus

--- app/javascript/controllers/interview_report_form_controller.js
+++ app/javascript/controllers/interview_report_form_controller.js
@@ -2,14 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static get targets () {
-    return [ "togglable" ]
+    return [ "analytics", 'answers' ]
   }
 
-  showTogglable() {
-    this.togglableTarget.classList.remove('d-none')
-  }
+  // showTogglable() {
+  //   this.togglableTarget.classList.remove('d-none')
+  // }
+  //
+  // hideTogglable() {
+  //   this.togglableTarget.classList.add('d-none')
+  // }
 
-  hideTogglable() {
-    this.togglableTarget.classList.add('d-none')
+  toggle() {
+    if (this.analyticsTarget.classList.contains('d-none')) {
+      this.analyticsTarget.classList.remove('d-nome')
+      this.answersTarget.classList.add('d-none')
+    } else if (this.answersTarget.classList.contains('d-none')) {
+      this.answersTarget.classList.remove('d-none')
+      this.analyticsTarget.classList.add('d-nome')
+    }
   }
 }
