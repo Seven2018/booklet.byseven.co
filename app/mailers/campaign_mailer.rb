3971e11e59ee86f339e7b0c6b7976b57a7674eae
Brice Chapuis <bricechapuis>
2022-04-01 16:26:21 +0200

    Interview emails done

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -2,7 +2,7 @@ class CampaignMailer < ApplicationMailer
   default from: CompanyInfo.no_reply
 
   def invite_employee(interviewer, interviewee, interview)
-    interviewee_email_settings(interviewer, interviewee, interview)
+    interview_email_settings(interviewer, interviewee, interview)
     mail(to: @interviewee.email, subject: "You are invited to the campaign '#{@interview.campaign.title}' !")
   end
 
@@ -14,20 +14,36 @@ class CampaignMailer < ApplicationMailer
   end
 
   def interview_reminder(interviewer, interviewee, interview)
-    interviewee_email_settings(interviewer, interviewee, interview)
+    interview_email_settings(interviewer, interviewee, interview)
     mail(to: @interviewee.email, subject: "Don't forget your interview !")
   end
 
   def interview_reminder_time(interviewer, interviewee, interview, time_remaining)
-    interviewee_email_settings(interviewer, interviewee, interview)
+    interview_email_settings(interviewer, interviewee, interview)
     @time_remaining = time_remaining
     @days_left = @time_remaining > 0 ? (@time_remaining.to_s + "days left") : "last day"
     mail(to: @interviewee.email, subject: "#{@days_left} to complete your interview !")
   end
 
+  def interview_set_completed_interviewee(interviewer, interviewee, interview)
+    interview_email_settings(interviewer, interviewee, interview)
+    mail(to: @interviewee.email, subject: "Congratulation, ‘#{@interview.campaign.title}’ is completed !")
+  end
+
+  def interview_set_completed_interviewer(interviewer, interviewee, interview)
+    interview_email_settings(interviewer, interviewee, interview)
+    mail(to: @interviewer.email, subject: "Interviews completed with #{@interviewee.fullname} !")
+  end
+
+  def campaign_completed(interviewer, campaign)
+    @interviewer = interviewer
+    @campaign = campaign
+    mail(to: @interviewer.email, subject: "Campaign completed !")
+  end
+
   private
 
-  def interviewee_email_settings(interviewer, interviewee, interview)
+  def interview_email_settings(interviewer, interviewee, interview)
     @interviewee = interviewee
     @interviewer = interviewer
     @interview = interview
