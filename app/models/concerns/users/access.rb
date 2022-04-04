563b948da40fe9cf607ee873c7222c861a6b1bd6
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-04-04 21:44:06 +0200

    Replace User::Access getters with :access_level_int enum getters

--- app/models/concerns/users/access.rb
+++ app/models/concerns/users/access.rb
@@ -30,37 +30,6 @@ module Users::Access
     ACCESS_LEVELS.include? access_level
   end
 
-  def admin
-    access_level == 'Super Admin'
-  end
-  alias admin? admin
-  alias super_admin admin
-  alias super_admin? admin
-
-  def account_owner?
-    access_level == 'Account Owner'
-  end
-
-  def hr?
-    access_level == 'HR'
-  end
-
-  def manager?
-    access_level == 'Manager'
-  end
-
-  def hr_light?
-    access_level == 'HR-light'
-  end
-
-  def manager_light?
-    access_level == 'Manager-light'
-  end
-
-  def employee?
-    access_level == 'Employee'
-  end
-
   def employee_to_hr_light?
     [
       'HR-light',
