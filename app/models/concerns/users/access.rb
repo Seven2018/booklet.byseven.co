c610fc5789c9ff50f70d9e9f7c6f1909c8ff7cca
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-04-04 21:47:25 +0200

    Deprecate unused :employee_to_hr_light?

--- app/models/concerns/users/access.rb
+++ app/models/concerns/users/access.rb
@@ -20,12 +20,4 @@ module Users::Access
   def employee_or_above?
     employee? || manager_or_above?
   end
-
-  def employee_to_hr_light?
-    [
-      'HR-light',
-      'Manager-light',
-      'Employee'
-    ].include? access_level
-  end
 end
