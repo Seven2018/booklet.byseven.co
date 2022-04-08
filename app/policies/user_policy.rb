f10f4ea42a8e3cac673661828f94daf56d35b302
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-04-08 15:58:30 +0200

    Refacto permissions/can_edit_employees_spec

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -20,6 +20,15 @@ class UserPolicy < ApplicationPolicy
     true
   end
 
+  def edit?
+    user.can_edit_employees ||
+      user.id == record.id
+  end
+
+  def update?
+    edit?
+  end
+
   def complete_profile?
     true
   end
@@ -29,22 +38,18 @@ class UserPolicy < ApplicationPolicy
   end
 
   def unlink_from_company?
-    user.hr_or_above?
-  end
-
-  def update?
-    true
+    create?
   end
 
   def destroy?
-    user.hr_or_above?
+    create?
   end
 
   def recommendation?
-    user.hr_or_above?
+    create?
   end
 
   def book_users?
-    user.hr_or_above?
+    create?
   end
 end
