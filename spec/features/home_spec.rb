fb6d90407bdc4e133e27be7544ba7f84544be19a
Hadrien Matringe <hadrien.matringe@gmail.com>
2022-04-08 16:01:44 +0200

    Update home_spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature 'Home', type: :feature, js: true do
   context 'a not signed in user' do
     scenario 'should not be able to browse the home page' do
       visit root_path
-      expect(page).to have_selector('#container-login')
+      expect(page).to have_selector("[name='user[email]'")
     end
   end
 
