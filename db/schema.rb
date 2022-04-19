7e7568b9ac2b7671cbec0975a69cce30fa9a7e99
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-04-19 17:33:47 +0200

    feat done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_160916) do
+ActiveRecord::Schema.define(version: 2022_04_19_133949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 2022_04_09_160916) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "applications", default: [], array: true
+    t.text "rating_logo", default: "<span class=\"iconify\" data-icon=\"clarity:star-solid\"></span>"
   end
 
   create_table "content_categories", force: :cascade do |t|
