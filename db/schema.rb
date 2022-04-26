2b17c1b068f1620534411bea554a5aae0c73494f
harold armijo leon <harmijoleon@gmail.com>
2022-04-26 15:51:04 +0200

    upload photo done

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_04_15_145357) do
+ActiveRecord::Schema.define(version: 2022_04_22_160157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -261,7 +260,6 @@ ActiveRecord::Schema.define(version: 2022_04_15_145357) do
     t.text "options"
     t.string "question_type"
     t.integer "position"
-    t.boolean "required", default: false
     t.boolean "allow_comments", default: false
     t.bigint "interview_form_id"
     t.datetime "created_at", precision: 6, null: false
@@ -292,7 +290,6 @@ ActiveRecord::Schema.define(version: 2022_04_15_145357) do
   create_table "interviews", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", default: ""
-    t.boolean "completed", default: false
     t.string "label", null: false
     t.date "date"
     t.time "starts_at"
@@ -305,6 +302,7 @@ ActiveRecord::Schema.define(version: 2022_04_15_145357) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "locked_at"
     t.bigint "interviewer_id"
+    t.integer "status", default: 0
     t.index ["campaign_id"], name: "index_interviews_on_campaign_id"
     t.index ["creator_id"], name: "index_interviews_on_creator_id"
     t.index ["employee_id"], name: "index_interviews_on_employee_id"
@@ -391,7 +389,7 @@ ActiveRecord::Schema.define(version: 2022_04_15_145357) do
 
   create_table "training_reports", force: :cascade do |t|
     t.bigint "company_id", null: false
-    t.jsonb "data", default: {}, null: false
+    t.jsonb "data", default: {}
     t.integer "state", default: 0, null: false
     t.integer "mode", default: 0, null: false
     t.datetime "start_time"
