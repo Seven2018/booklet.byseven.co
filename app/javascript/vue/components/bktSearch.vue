790ac5e2815071d0492a93652629ae786303454f
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-05-06 15:54:05 +0200

    store implemantation

--- app/javascript/vue/components/bktSearch.vue
+++ app/javascript/vue/components/bktSearch.vue
@@ -2,24 +2,22 @@
   <div class="position-relative">
     <input placeholder="Search" class="p-3 bkt-bg-light-grey3 rounded-5px" @input="update">
     <span class="iconify bkt-light-grey" data-icon="entypo:magnifying-glass" data-width="30" data-height="30"></span>
-    {{search}}
+<!--    {{search.value}}-->
   </div>
 </template>
 
 <script>
-import store from "../store/search";
+import store from "../store";
 
 export default {
-  computed: {
-    search() {
-      console.log('update', store.state.search)
-      return store.state.search
+  data() {
+    return {
+      search: store.state.search
     }
   },
   methods: {
     update(e) {
-      store.commit('setSearch', e.target.value)
-      console.log('setSearch', store.state.search)
+      store.commit('search/set', e.target.value)
     }
   }
 }
