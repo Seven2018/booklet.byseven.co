2755390cca9eb596ab2b30aba7357cd039b4d6b0
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-05-06 18:04:40 +0200

    index element not done

---
+++ app/javascript/vue/components/IndexTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <table class="">
+    <thead>
+    <tr>
+      <th v-for="col in headers">
+        <p class="font-weight-500">
+          {{ col }}
+        </p>
+      </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="row in tableData" class="border-bottom-bkt-light-grey5-not-last-child ">
+      <slot v-bind="row"></slot>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: ['headers', 'tableData']
+}
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+table td {
+  padding-top: 17px;
+  padding-bottom: 17px;
+}
+
+</style>
--- app/javascript/vue/pages/objectives/index.vue
+++ app/javascript/vue/pages/objectives/index.vue
@@ -10,13 +10,64 @@
         </bkt-button>
       </div>
     </div>
+
     <bkt-box>
-      <div class="flex-row-start-centered">
+      <div class="flex-row-start-centered mb-5">
         <bkt-search></bkt-search>
         <p class="ml-5 font-weight-500">
           Reset
         </p>
       </div>
+
+      <index-table
+          :headers="headers"
+          :tableData="tableData">
+        <template v-slot="{name, jobTitle, accessLevel, manager, objectives, objectivesTeamLink, objectiveLink}">
+          <td>
+            <div class="d-flex align-items-center">
+              <div class="flex-column ">
+                <img class="rounded-circle width-3rem height-3rem" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png" alt="">
+              </div>
+
+              <div class="flex-column ml-3">
+                <div class="flex-row-start-centered">
+                  <p class="font-weight-500">{{name}}</p>
+                </div>
+                <div class="flex-row-start-centered">
+                  <p class="font-weight-500 fs-1_2rem bkt-light-grey6">{{jobTitle}}</p>
+                </div>
+              </div>
+            </div>
+          </td>
+
+          <td>
+            <p>
+              {{accessLevel}}
+            </p>
+          </td>
+
+          <td>
+            <p>
+              {{manager}}
+            </p>
+          </td>
+
+
+          <td>
+            <p>
+              {{objectives}}
+            </p>
+          </td>
+
+          <td>
+            <p>
+              {{objectivesTeamLink}}
+
+              {{objectiveLink}}
+            </p>
+          </td>
+        </template>
+      </index-table>
     </bkt-box>
   </div>
 </template>
@@ -25,12 +76,23 @@
 import BktButton from '../../components/BktButton';
 import BktBox from "../../components/BktBox";
 import BktSearch from "../../components/bktSearch";
+import IndexTable from '../../components/IndexTable'
 
 export default {
+  data() {
+    return {
+      headers: ['Name', 'Access Level', 'Manager', 'Objectives', ''],
+      tableData: [
+        {name: 'Marvin', jobTitle: 'Web Designer', accessLevel: 'Manager', manager: {image: '', name: 'Ralph Edwards'}, objectives: 4, objectivesTeamLink: 'teamlink', objectiveLink: 'link'},
+        {name: 'Marvin', jobTitle: 'Web Designer', accessLevel: 'Manager', manager: {image: '', name: 'Ralph Edwards'}, objectives: 4, objectivesTeamLink: 'teamlink', objectiveLink: 'link'},
+      ]
+    }
+  },
   components: {
     BktSearch,
     BktBox,
     BktButton,
+    IndexTable
   }
 }
 </script>
