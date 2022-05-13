42d48b3162b7c634b978214f99eadc758b8d3f6e
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-05-13 18:57:14 +0200

    link objectives

--- app/javascript/vue/pages/objectives/users/ObjectivesUserTable.vue
+++ app/javascript/vue/pages/objectives/users/ObjectivesUserTable.vue
@@ -6,52 +6,60 @@
   >
     <template v-slot="{id, title, due_date, objective_indicator}">
       <td>
-        <div class="flex-row-start-centered max-w-25rem">
-          <div v-if="isCompleted(objective_indicator)">
+        <a :href="$routes.generate('objective_elements_id', {id})">
+          <div class="flex-row-start-centered max-w-25rem">
+            <div v-if="isCompleted(objective_indicator)">
             <span  class="iconify mr-2 bkt-light-grey5" data-width="20"
-                  data-icon="akar-icons:check"></span>
+                   data-icon="akar-icons:check"></span>
+            </div>
+            <p
+                class="font-weight-500 text-truncate"
+                :class="{'bkt-light-grey5': isCompleted(objective_indicator)}"
+            >{{ title }}</p>
           </div>
-          <p
-              class="font-weight-500 text-truncate"
-              :class="{'bkt-light-grey5': isCompleted(objective_indicator)}"
-          >{{ title }}</p>
-        </div>
+        </a>
       </td>
 
       <td>
-        <p
-            class="font-weight-500"
-            :class="{'bkt-light-grey5': isCompleted(objective_indicator)}"
-        >
+        <a :href="$routes.generate('objective_elements_id', {id})">
+          <p
+              class="font-weight-500"
+              :class="{'bkt-light-grey5': isCompleted(objective_indicator)}"
+          >
           <span
               v-if="objective_indicator.indicator_type === 'boolean'"
           >
-            actual value: {{ objective_indicator.options.starting_value }}, target value: {{ objective_indicator.options.target_value }}
+            actual value: {{
+              objective_indicator.options.starting_value
+            }}, target value: {{ objective_indicator.options.target_value }}
           </span>
-          <span
-              v-else-if="objective_indicator.indicator_type === 'numeric_value'"
-          >
+            <span
+                v-else-if="objective_indicator.indicator_type === 'numeric_value'"
+            >
             {{ objective_indicator.options.starting_value }}/{{ objective_indicator.options.target_value }}
           </span>
-          <span
-              v-else-if="objective_indicator.indicator_type === 'percentage'"
-          >
+            <span
+                v-else-if="objective_indicator.indicator_type === 'percentage'"
+            >
             {{ objective_indicator.options.target_value }}%
           </span>
-          <span
-              v-else-if="objective_indicator.indicator_type === 'multi_choice'"
-          >
+            <span
+                v-else-if="objective_indicator.indicator_type === 'multi_choice'"
+            >
             <span v-if="!objective_indicator.options.starting_value">Not set yet</span>
             <span v-else>{{ objective_indicator.options.starting_value }}</span>
           </span>
-        </p>
+          </p>
+        </a>
       </td>
 
       <td>
-        <p
-            class="font-weight-500 text-truncate"
-            :class="{'bkt-light-grey5': isCompleted(objective_indicator)}"
-        >{{ due_date }}</p>
+        <a :href="$routes.generate('objective_elements_id', {id})">
+          <p
+              class="font-weight-500 text-truncate"
+              :class="{'bkt-light-grey5': isCompleted(objective_indicator)}"
+          >{{ due_date }}</p>
+        </a>
       </td>
 
       <td v-if="showOptions">
