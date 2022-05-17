bd7c851bc36f2c16a5159a43d64ca2495e4843e4
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-05-17 15:23:45 +0200

    test capybara system

--- spec/system/users/users_sessions_spec.rb
+++ spec/system/users/users_sessions_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Users::Sessions", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  # before do
+  #   driven_by(:rack_test)
+  # end
 
   it '/u/sign_in Get sign in page' do
     visit '/u/sign_in'
 
-    expect(page).to have_http_status(:ok)
+    find("[name='user[email]']")
+    # expect(page).to have_http_status(:ok)
   end
 end
