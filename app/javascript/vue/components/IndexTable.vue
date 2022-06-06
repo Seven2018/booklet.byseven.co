38a8ca4ce5c6b84382c96f6703d7aae813ac2805
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-06-06 17:25:42 +0200

    fix pagegination color

--- app/javascript/vue/components/IndexTable.vue
+++ app/javascript/vue/components/IndexTable.vue
@@ -37,21 +37,21 @@
       <div class="d-flex">
 <!--        before-->
         <a
-            class="bkt-objective-blue3 mx-2"
+            class="bkt-objective-blue3-important mx-2"
             v-for="n in makeRange(0, pagination.current_page)"
             @click="$emit('fetch-page', n)"
         >{{ n }}</a>
         <span
             v-if="makeRange(0, pagination.current_page).length && !makeRange(0, pagination.current_page).includes(pagination.current_page - 1)"
-            class="bkt-objective-blue3 mx-2">...</span>
+            class="bkt-objective-blue3-important mx-2">...</span>
 <!--        current-->
         <a
-            class="bkt-objective-blue mx-2"
+            class="bkt-objective-blue-important mx-2"
             @click="$emit('fetch-page', pagination.current_page)"
         >
           {{pagination.current_page}}</a>
         <a
-            class="bkt-objective-blue3 mx-2"
+            class="bkt-objective-blue3-important mx-2"
             v-for="n in makeRange(pagination.current_page, pagination.total_pages)"
             @click="$emit('fetch-page', n)"
         >{{ n }}</a>
@@ -60,10 +60,10 @@
             v-if="!makeRange(pagination.current_page, pagination.total_pages).includes(pagination.total_pages - 1)
             && pagination.current_page != pagination.total_pages - 1
             && pagination.current_page < pagination.total_pages"
-            class="bkt-objective-blue3 mx-2">...</span>
+            class="bkt-objective-blue3-important mx-2">...</span>
         <a
             v-if="pagination.current_page < pagination.total_pages"
-            class="bkt-objective-blue3 mx-2"
+            class="bkt-objective-blue3-important mx-2"
             @click="$emit('fetch-page', pagination.total_pages)"
         >
           {{pagination.total_pages}}</a>
