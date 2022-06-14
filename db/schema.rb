2ec9735c7bb04eeaf8a128c24c965bfab534b4da
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-06-14 09:10:53 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -450,7 +450,7 @@ ActiveRecord::Schema.define(version: 2022_06_13_154541) do
 
   create_table "training_reports", force: :cascade do |t|
     t.bigint "company_id", null: false
-    t.jsonb "data", default: {}
+    t.jsonb "data", default: {}, null: false
     t.integer "state", default: 0, null: false
     t.integer "mode", default: 0, null: false
     t.datetime "start_time"
@@ -630,6 +630,7 @@ ActiveRecord::Schema.define(version: 2022_06_13_154541) do
   add_foreign_key "interviews", "users", column: "interviewer_id"
   add_foreign_key "mods", "companies"
   add_foreign_key "objective_elements", "companies"
+  add_foreign_key "objective_elements", "users", column: "creator_id"
   add_foreign_key "objective_indicators", "objective_elements"
   add_foreign_key "objective_logs", "objective_elements"
   add_foreign_key "objective_logs", "objective_indicators"
