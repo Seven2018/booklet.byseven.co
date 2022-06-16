e45faf1b0f65bd4d17530d39b895b051ff435238
Harold Armijo Leon <harold.armijo-leon@epitech.eu>
2022-06-16 18:38:59 +0200

    some front

--- app/javascript/vue/components/GroupCategoryFilter.vue
+++ app/javascript/vue/components/GroupCategoryFilter.vue
@@ -1,36 +1,55 @@
-
 <template>
   <div>
     <div class="flex-row-start-centered mt-5">
       <h2 class="flex-row-start-centered align-items-center font-weight-600 fs-1_6rem">
         Filter by tags
-        <div
+        <span
             @click="openInfo"
         >
           <span
               class="iconify ml-3 cursor-pointer"
               data-icon="akar-icons:question"
           ></span>
-        </div>
+        </span>
       </h2>
     </div>
 
     <div
-        v-for="item in groupTagModule.groups"
-        :key="item.key"
+        v-for="(item, idx) in showGroups"
+        :key="item.id"
+        :ref="loopCatSuggestionRef + idx"
         class="flex-row-between-centered mt-5">
       <bkt-button
           type="transparent"
-          class="mr-2 width-25" style="display: flex; justify-content: flex-start; align-items: center;"
+          class="mr-2 width-25 min-h-5rem" style="display: flex; justify-content: flex-start; align-items: center;"
       >
-        Others
+        {{ item.name | capitalize }}
       </bkt-button>
-      <div class=" ml-2 bkt-bg-light-grey9-hover px-3 rounded-5px width-75">
+      <div
+          class=" ml-2 bkt-bg-light-grey9-hover px-3 rounded-5px width-75 min-h-5rem"
+          @click="editTagField(idx)"
+          @mouseover="showCatSuggestion(idx)"
+          @mouseleave="hideCatSuggestion(idx)"
+      >
         <bkt-tag
             v-for="tag in item.categories"
-            class="mx-2 d-inline-block my-3"
+            class="mx-2 d-inline-block my-3 vertical-align-middle"
             :selected="false"
-        >{{tag.title}}</bkt-tag>
+            @click.stop="() => {}"
+        >{{ tag.title }}
+        </bkt-tag>
+        <p
+            v-if="item.showCatSuggestion && item.showInputCatSuggestion === false"
+            class="bkt-light-grey6 my-3 ml-3 cursor-pointer d-inline-block vertical-align-middle"
+        >
+          Move an existing tag to this category
+        </p>
+        <input
+            v-if="item.showInputCatSuggestion"
+            @blur="resetCatSuggestion(idx)"
+            type="text"
+            class="border-none bg-transparent input-cat-suggestion d-inline-block my-3"
+        >
       </div>
     </div>
 
@@ -54,11 +73,25 @@ import BktTag from "./BktTag";
 export default {
   data() {
     return {
-      groupTagModule: store.state.groupsTag
+      groupTagModule: store.state.groupsTag,
+      loopCatSuggestionRef: 'loopCatSuggestion'
     }
   },
   created() {
     store.dispatch('groupsTag/fetch', {kind: 'interview'})
+  },
+  computed: {
+    showGroups() {
+      if (!this.groupTagModule.groups || this.groupTagModule.groups.length === 0) return []
+
+      // if ('showCatSuggestion' in this.groupTagModule.groups[0]) return this.groupTagModule.groups
+      if (!('showCatSuggestion' in this.groupTagModule.groups[0])) {
+        this.groupTagModule.groups = this.groupTagModule.groups.map((item) => {
+          return {...item, showCatSuggestion: false, showInputCatSuggestion: false}
+        })
+      }
+      return this.groupTagModule.groups
+    }
   },
   methods: {
     openInfo() {
@@ -67,7 +100,25 @@ export default {
         componentName: 'info-filter-category-tags',
         closable: true,
       })
-    }
+    },
+    editTagField(arrayIdx) {
+      this.groupTagModule.groups[arrayIdx].showInputCatSuggestion = true
+      this.groupTagModule.groups[arrayIdx].showCatSuggestion = false
+      this.$nextTick(() => {
+        const input = this.$refs[this.loopCatSuggestionRef + arrayIdx][0].querySelector('.input-cat-suggestion')
+        input.focus()
+      })
+    },
+    resetCatSuggestion(arrayIdx) {
+      this.groupTagModule.groups[arrayIdx].showInputCatSuggestion = false
+      this.groupTagModule.groups[arrayIdx].showCatSuggestion = false
+    },
+    showCatSuggestion(arrayIdx) {
+      this.groupTagModule.groups[arrayIdx].showCatSuggestion = true
+    },
+    hideCatSuggestion(arrayIdx) {
+      this.groupTagModule.groups[arrayIdx].showCatSuggestion = false
+    },
   },
   components: {BktTag, BktButton},
 }
