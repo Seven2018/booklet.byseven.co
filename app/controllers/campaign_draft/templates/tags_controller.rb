df549cf2d388d6e614eda13a10f826f3133378d7
Brice Chapuis <bricechapuis>
2022-06-21 16:57:33 +0200

    misc fixes

--- app/controllers/campaign_draft/templates/tags_controller.rb
+++ app/controllers/campaign_draft/templates/tags_controller.rb
@@ -8,7 +8,10 @@ class CampaignDraft::Templates::TagsController < CampaignDraft::BaseController
   private
 
   def tags
+    users_ids = CampaignDraft.find(params[:campaign_draft_id]).interviewee_ids
+
     Tag.where(company: current_user.company, tag_category_id: params[:search])
+       .where_exists(:user_tags, user_id: users_ids)
        .order(tag_name: :asc)
        .select(:id, :tag_name)
   end
