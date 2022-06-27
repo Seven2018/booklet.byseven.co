10b3c49de87c3a083c87eb4db053ebdfb072c731
Legrand <arl.lgdm@gmail.com>
2022-06-27 17:42:38 +0200

    catch up pagination style

---
+++ app/javascript/vue/components/table/pagination/NextButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <bkt-button
+      v-if="pagination.next_page"
+      @click="method(pagination.next_page)"
+    >
+      <div class="paginate__next-button paginate__button-blue">
+        <p>Next</p>
+        <span class="iconify" data-icon="fontisto:angle-right"></span>
+      </div>
+  </bkt-button>
+</template>
+
+<script>
+import BktButton from "../../BktButton";
+
+export default {
+  components: {BktButton},
+  props: {
+    pagination: Object,
+    method: { type: Function },
+  }
+}
+</script>
---
+++ app/javascript/vue/components/table/pagination/PreviousButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <bkt-button
+        v-if="pagination.prev_page"
+        @click="method(pagination.prev_page)"
+    >
+    <div class='paginate__previous-button paginate__button-blue'>
+      <span class="iconify" data-icon="fontisto:angle-left"></span>
+      <p>Previous</p>
+    </div>
+  </bkt-button>
+</template>
+
+<script>
+import BktButton from "../../BktButton";
+
+export default {
+  components: {BktButton},
+  props: {
+    pagination: Object,
+    method: { type: Function },
+  }
+}
+</script>
